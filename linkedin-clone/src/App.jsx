6228be353e282c07452108a13e43e75d57db4034
Om Prakash Sahoo <omprakashsahoo1234@gmail.com>
2024-06-13 22:28:58 +0530

    Initialized repository with necessary modules and redux and firebase

---
+++ linkedin-clone/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-pink-700">Making a linkedin clone</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ linkedin-clone/src/Firebase.jsx
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsDQIJM6wiRSF7kXS-zo3b5dZomxFrIPs",
+  authDomain: "linkedin-clone-c1c64.firebaseapp.com",
+  projectId: "linkedin-clone-c1c64",
+  storageBucket: "linkedin-clone-c1c64.appspot.com",
+  messagingSenderId: "944862144258",
+  appId: "1:944862144258:web:511d2b90c1908bfe75c41b",
+  measurementId: "G-4EQVEN5BGD",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+
+//add this line in codes where you need it =>
+// import firebase from "./Firebase.jsx"
+
+
